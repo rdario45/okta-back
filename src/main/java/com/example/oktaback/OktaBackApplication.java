11ad1897783e3cb8829771859411e1a9980edee6
Ruben <rdario45@gmail.com>
2024-05-03 06:37:59 -0500

    initial commit

---
+++ src/main/java/com/example/oktaback/OktaBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.oktaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OktaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oktaback/OktaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oktaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
